4a72fd3c0eabc8c4cf06368b76b9c6d03030bc50
pablo <pablo@yenni>
2017-05-03 03:03:10 -0300

    Solución error en menú hornereando.

--- protected/views/torneo/view.php
+++ protected/views/torneo/view.php
@@ -14,7 +14,8 @@ $this->menu = array(
         'linkOptions' => array('submit' => array('borrarinscripcion', 'idTorneo' => $model->idTorneo), 'confirm' => 'Está seguro de borrar la Inscripción a este Torneo? Perderá todas sus soluciones.'),
         
         'visible' => $model->getUsuarioInscripcion()!=FALSE),
-    array('label' => 'Hornereando', 'url' => array('hornereando', 'idTorneo'=>$model->idTorneo), 'visible' => $model->idTipo==4),//Preguntados
+    array('label' => 'Hornereando', 'url' => array('hornereando', 'idTorneo'=>$model->idTorneo),
+        'visible' => $model->idTipo==4 && $model->getUsuarioInscripcion()!=FALSE),//Preguntados
     array('label' => 'Create Torneo', 'url' => array('create'), 'visible' => Yii::app()->user->checkAccess('Administrador')),
     array('label' => 'Update Torneo', 'url' => array('update', 'id' => $model->idTorneo), 'visible' => Yii::app()->user->checkAccess('Administrador')),
     array('label' => 'Manejar Problemas', 'url' => array('/torneoProblema/admin', 'id' => $model->idTorneo), 'visible' => Yii::app()->user->checkAccess('Administrador')),
